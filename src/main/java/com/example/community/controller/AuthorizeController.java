305a584494d570d77419f19faf4b3316b53e2240
chrislu732 <chrislu732@gmail.com>
2020-12-30 01:49:42 -0500

    github user authorize

---
+++ src/main/java/com/example/community/controller/AuthorizeController.java
@@ -0,0 +1,44 @@
+package com.example.community.controller;
+
+import com.example.community.dto.AccessTokenDTO;
+import com.example.community.dto.GithubUser;
+import com.example.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+// the page after signing in
+@Controller
+public class AuthorizeController {
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.url}")
+    private String redirectUrl;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state") String state) throws Exception {
+        // set the information of the oauth app
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_url(redirectUrl);
+        accessTokenDTO.setState(state);
+        // get github access token
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        // get user information
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getLogin());
+        return "index";
+    }
+
+}
--- src/main/java/com/example/community/controller/IndexController.java
+++ src/main/java/com/example/community/controller/IndexController.java
@@ -3,9 +3,9 @@ package com.example.community.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+// main page
 @Controller
 public class IndexController {
-
     @GetMapping("/")
     public String index() {
         return "index";
